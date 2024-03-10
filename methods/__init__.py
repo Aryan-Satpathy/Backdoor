76fd9d55cdff440395de30d65a49f44fed5885d9
Dhruva Rajwade <75108925+DhruvaRajwade@users.noreply.github.com>
2024-03-11 00:12:02 +0530

    Update __init__.py

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,11 +1,21 @@
 from .SimCLR.simclr import SimCLRModel
 from .BYOL.byol import BYOL
+from .MoCo.mocov3 import MoCoV3
+from .SimSiam.simsiam import SimSiam
 
 def set_model(args):
     if args.method == 'simclr':
         return SimCLRModel(args)
+    
     elif args.method == 'byol':
         return BYOL(args)
+    
+    elif args.method=='moco':
+        return MoCoV3(args)
+
+    elif args.method=='simsiam':
+        return SimSiam(args)
+
     else:
         raise  NotImplementedError
 
