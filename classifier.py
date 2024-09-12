5e40d4836aad47bcc023ae18b58450dbe6a7ba26
hskalin <nilaksh00072@gmail.com>
2024-09-12 13:31:55 +0000

    removed cifar hardcode

--- classifier.py
+++ classifier.py
@@ -165,7 +165,10 @@ def main_worker(gpu,  args):
         transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]),
     ])
 
-    train_dataset = torchvision.datasets.CIFAR10(root='data', train=True, download=True, transform=train_transform)
+    if args.dataset == 'cifar10':
+        train_dataset = torchvision.datasets.CIFAR10(root='data', train=True, download=True, transform=train_transform)
+    else:
+        train_dataset = torchvision.datasets.CIFAR100(root='data', train=True, download=True, transform=train_transform)
     train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=16)
 
 
--- robust.py
+++ robust.py
@@ -206,7 +206,10 @@ def main_worker(gpu,  args):
         transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]),
     ])
 
-    test_dataset = torchvision.datasets.CIFAR10(root='data', train=False, download=True, transform=test_transform)
+    if args.dataset == 'cifar10':
+        test_dataset = torchvision.datasets.CIFAR10(root='data', train=False, download=True, transform=test_transform)
+    else:
+        test_dataset = torchvision.datasets.CIFAR100(root='data', train=False, download=True, transform=test_transform)
     test_loader = DataLoader(test_dataset, batch_size=128, shuffle=True, num_workers=16)
 
     acc = runAA(model.eval(), test_loader, log_path=f'logs/{RUNNAME}', advFlag=None)
